2d52458fd4042c453e844180e3f05de24bd70f0b
glottis <skin_tiicket@hotmail.com>
2022-11-27 22:20:41 +0100

    init

---
+++ lib/ds18b20_1w.ex
@@ -0,0 +1,59 @@
+defmodule Ds18b201w do
+  @moduledoc """
+  Documentation for Ds18b201w.
+  """
+
+  @doc """
+  Lists all connected ds18b201w sensors
+  """
+  def list_sensors do
+    Path.wildcard("/sys/bus/w1/devices/28-*")
+  end
+
+  @doc """
+  Reads all connected ds18b201w sensors
+  """
+  def read_sensors do
+    list_sensors() |> Enum.map(&read_temperature_file/1)
+  end
+
+  @doc """
+  Reads and returns sensor file data along with sensor id
+
+  Example:
+
+  iex> Ds18b201w.read_temperature_file("/sys/bus/w1/devices/28-01203d2f1e12")
+
+  {:ok, 28-01203d2f1e12, "01 00 4b 46 7f ff 0c 10 8b : crc=8b YES\n01 00 4b 46 1f ff 0c 10 8b t=62\n"}
+
+  """
+  def read_temperature_file(sensor_path) do
+    sensor_id = sensor_path |> Path.split() |> List.last()
+
+    case File.read(sensor_path <> "/w1_slave") do
+      {:ok, data} -> data |> parse_temperature_file()
+      {:error, error_msg} -> {:error, sensor_id, error_msg}
+    end
+  end
+
+  def parse_temperature_file(data) do
+    with true <- valid_crc?(data),
+         raw_temp <-
+           data
+           |> String.split()
+           |> Enum.reverse()
+           |> List.first()
+           |> String.split("=")
+           |> List.last()
+           |> String.to_integer(),
+         temperature <- (raw_temp / 100) |> Float.round(1) do
+      {:ok, temperature}
+    else
+      _ -> {:error, :invalid_crc}
+    end
+  end
+
+  defp valid_crc?(crc) do
+    crc |> String.split() |> Enum.member?("YES")
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Ds18b201w.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ds18b20_1w,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+end
