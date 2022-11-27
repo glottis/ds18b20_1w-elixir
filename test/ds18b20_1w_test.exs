543247cf9710c8d332a573d7e5d9c8e498644985
glottis <skin_tiicket@hotmail.com>
2022-11-27 23:03:31 +0100

    TDD all the way

--- test/ds18b20_1w_test.exs
+++ test/ds18b20_1w_test.exs
@@ -15,23 +15,23 @@ defmodule Ds18b201wTest do
 
   test "Check valid temperature output" do
     assert Ds18b201w.parse_temperature_file(
-             "01 00 4b 46 7f ff 0c 10 8b : crc=8b YES 01 00 4b 46 1f ff 0c 10 8b t=1050 "
+             "01 00 4b 46 7f ff 0c 10 8b : crc=8b YES 01 00 4b 46 1f ff 0c 10 8b t=10500 "
            ) == {:ok, 10.5}
 
     assert Ds18b201w.parse_temperature_file(
-             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=-2050 "
+             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=-20500 "
            ) == {:ok, -20.5}
 
     assert Ds18b201w.parse_temperature_file(
-             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=1530 "
+             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=15300"
            ) == {:ok, 15.3}
 
     assert Ds18b201w.parse_temperature_file(
-             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=830 "
+             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=8300"
            ) == {:ok, 8.3}
 
     assert Ds18b201w.parse_temperature_file(
-             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=-130 "
+             "01 00 4b 46 7f ff 0c 11 8b : crc=8f YES 01 00 4b 46 1f ff 0c 10 8b t=-1300"
            ) == {:ok, -1.3}
   end
 end
